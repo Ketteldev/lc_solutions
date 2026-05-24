5daa9a2a6a299d4bdf27af4781f2ba72179fb98c
ketteldev <bshane@pm.me>
2026-05-24 16:08:25 -0400

    Question 1340 Solution

--- q1340/elixir/solution.exs
+++ q1340/elixir/solution.exs
@@ -1,4 +1,26 @@
 defmodule Solution do
+  @moduledoc """
+  First try. No "gotcha" edge cases today, which felt really good to accomplish.
+  Took a little extra time to think about strategy, and just thought about
+  solving for local minimums, which is 1. Then, climbing up in value I could add
+  steps for each one that the index can reach. Since they cannot even jump over
+  higher points, we can just immediately stop if a pillar is unreachable.
+
+  My general strategy is to:
+    - Sort by value first, index second, so I go in order of indices that are
+      the lowest points and climbing upwards.
+    - Then, iterating over the list, checking left and right direction for valid
+      jumps, stopping immediately if invalid and not checking further.
+    - Then, checking the memory map, for previously discovered scores, and
+      taking the maximum from those and adding one to it for the current
+      position, then storing that in the memory map.
+    - Then, taking the max of the maxes.
+
+  I believe this falls under the paradigm of Dynamic Programming, solving the
+  smallest iteration and building up.
+
+  Also, it isn't the prettiest code, but it works well.
+  """
   @spec max_jumps(arr :: [integer], d :: integer) :: integer
   def max_jumps(arr, d) do
     tar = List.to_tuple(arr)
@@ -6,25 +28,39 @@ defmodule Solution do
 
     [{_value, index} | rest] = arr |> Enum.with_index() |> Enum.sort(&(&1 <= &2))
 
-    Enum.reduce(rest, %{index => 0}, fn {v, i}, acc ->
-      Enum.reduce(1..d, [], fn idx, a ->
+    Enum.reduce(rest, %{index => 1}, fn {_v, i}, acc ->
+      left = Enum.reduce_while(-1..(-1 * d)//-1, [], fn delta, acc ->
+        checkdex = i + delta
+
+        if checkdex > -1 and elem(tar, checkdex) < elem(tar, i) do
+          {:cont, [checkdex | acc]}
+        else
+          {:halt, acc}
+        end
+      end)
+
+      left_and_right = Enum.reduce_while(1..d, left, fn delta, acc ->
+        checkdex = i + delta
+
+        if checkdex < len and elem(tar, checkdex) < elem(tar, i) do
+          {:cont, [checkdex | acc]}
+        else
+          {:halt, acc}
+        end
+      end)
+
+      Enum.reduce(left_and_right, [1], fn idx, a ->
         case Map.get(acc, idx) do
-          nil -> [0 | a]
+          nil -> [1 | a]
           value -> [1 + value | a]
         end
       end)
       |> Enum.max()
       |> then(&Map.put(acc, i, &1))
     end)
-    |> IO.inspect()
-
-    0
+    |> Map.values()
+    |> Enum.max()
   end
-
-  defp smallest_possible(v) when v < 0, do: 0
-  defp smallest_possible(v), do: v
-  defp highest_possible(v, l) when v >= l, do: l - 1
-  defp highest_possible(v, _l), do: v
 end
 
 Solution.max_jumps([6,4,14,6,8,13,9,7,10,6,12], 2) |> IO.inspect(label: "Expected 4, got")
