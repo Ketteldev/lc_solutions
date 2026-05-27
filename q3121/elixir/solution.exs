f5e3a943a45fb0166edc9488124db35c36c5cfee
ketteldev <bshane@pm.me>
2026-05-27 09:57:02 -0400

    Question 3121 Solution
    Adding module doc, remove Enum.filter/2

--- q3121/elixir/solution.exs
+++ q3121/elixir/solution.exs
@@ -1,11 +1,23 @@
 defmodule Solution do
+  @moduledoc """
+  No gotchas again today. The approach is to make use of a map, using the
+  lowercase letter as the key. The values at the key are one of 3; `:upper` for
+  setting the expectation that it is waiting for the uppercase character to be
+  seen, `[UPPER]` when the uppercase character has been found, and `:bad` for
+  when either the uppercase has been seen first, or a lowercase has been seen
+  after the uppercase appearance.
+
+  Counting up to 26 possible key-value pairs results in O(26) worst case, ergo
+  the count is just O(1). So the total here would be O(n) for the string that is
+  passed in.
+  """
+
   @spec number_of_special_chars(word :: String.t) :: integer
   def number_of_special_chars(word) do
     word
     |> String.to_charlist()
     |> Enum.reduce(%{}, &handle_char/2)
-    |> Enum.filter(fn {k, v} -> k - 32 == v end)
-    |> Enum.count()
+    |> Enum.count(fn {k, v} -> k - 32 == v end)
   end
 
   defp handle_char(char, record) when char in ?A..?Z do
